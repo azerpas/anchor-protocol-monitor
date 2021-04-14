87b0eac4564e9d01a95ed1eddd0fb636e485d07b
azerpas <xskylix@hotmail.fr>
2021-04-14 23:06:57 +0200

    Added Discord

---
+++ discord.ts
@@ -0,0 +1,28 @@
+interface DiscordProps {
+    balance: number;
+}
+
+export const sendWebhook = async (props: DiscordProps) => {
+    const webhook = process.env.WEBHOOK;
+    if(!webhook) throw new Error(`Undefined .env value: 'WEBHOOK'`);
+    const data = JSON.stringify({
+        "content": null,
+        "embeds": [
+            {
+                "title": "BALANCE UPDATE 💰", "color": 7340287,
+                "fields": [
+                    {
+                        "name": "Balance","value": `${props.balance.toString()} aUST`,"inline": true
+                    },
+                ],
+                "footer": {  "text": "Anchor Protocol" }, "timestamp": new Date().toISOString()
+            }
+        ]
+    });
+    const requestOptions = {
+        method: 'POST',
+        headers: {"Content-Type": "application/json"},
+        body: data
+    };
+    await fetch(webhook, requestOptions);
+}
--- handler.ts
+++ handler.ts
@@ -1,11 +1,14 @@
 import { Lambda } from 'aws-sdk';
 import { APIGatewayEvent } from 'aws-lambda';
+import { sendWebhook } from './discord';
 
 
 export const handler = async (event: APIGatewayEvent) => {
     const arn = process.env.ARN;
     if(!arn) throw new Error(`Undefined .env value: 'arn'`)
-    if(!process.env.ACCESS_KEY || !process.env.SECRET_KEY) throw new Error(`Undefined .env value: '${process.env.SECRET_KEY}' '${process.env.ACCESS_KEY}'`)
+    if(!process.env.ACCESS_KEY || !process.env.SECRET_KEY) throw new Error(`Undefined .env value: '${process.env.SECRET_KEY}' '${process.env.ACCESS_KEY}'`);
+    if(!process.env.TERRA_ADDR) throw new Error(`Undefined .env value: 'TERRA_ADDR'`);
+    if(!process.env.WEBHOOK) throw new Error(`Undefined .env value: 'WEBHOOK'`);
     const params = {
       FunctionName: arn, 
       MemorySize: 256,
@@ -13,13 +16,18 @@ export const handler = async (event: APIGatewayEvent) => {
         Variables: {
           'LAST_AT': Date.now().toString(),
           'ACCESS_KEY': process.env.ACCESS_KEY,
-          'SECRET_KEY': process.env.SECRET_KEY
+          'SECRET_KEY': process.env.SECRET_KEY,
+          'ARN': arn,
+          'TERRA_ADDR': process.env.TERRA_ADDR,
+          'LAST_BALANCE': '0',
+          'WEBHOOK': process.env.WEBHOOK
         }
       }
     };
     const lambda = new Lambda({accessKeyId: process.env.ACCESS_KEY, secretAccessKey: process.env.SECRET_KEY,region: 'eu-west-3'});
     const data = await lambda.updateFunctionConfiguration(params).promise();
     console.info(data);
+    await sendWebhook({balance: 3240})
     return {
         statusCode: 200,
         body: {env: process.env.AWS_EXECUTION_ENV, els: 'he'},
